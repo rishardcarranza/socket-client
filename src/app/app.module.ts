b9053f8e90fa473692935bd3fef05f92f9e501c4
Richard Carranza <rishardcarranza@gmail.com>
2019-09-03 22:04:31 -0600

    First commit, angular app for test socket server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = {
+  url: environment.wsURL, options: {}
+};
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
